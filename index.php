01060ffda1618543e98d2e58629208c96176de3f
Elias <elias@dobrin.net>
2020-04-20 20:46:02 -0700

    redid data structure for worms and stuff

--- index.php
+++ index.php
@@ -30,6 +30,121 @@
         
         <script>
             
+            class Worm
+            {
+                controllable;
+                nodes = [];
+                color = "hsl(" + Math.round(Math.random() * 360) + ", 100%, 50%)";
+                camera = new Camera();
+                
+                constructor(type, count)
+                {
+                    if(type === "Human")
+                    {
+                        this.controllable = true;
+                    }
+                    
+                    if(type === "Bot")
+                    {
+                        this.controllable = false;
+                    }
+                    
+                    this.addNode(count);
+                }
+                
+                addNode(count)
+                {
+                    if(count === undefined)
+                    {
+                        count = 1;
+                    }
+                    
+                    var temp_length = this.nodes.length;
+                    
+                    for(var n = 0; n < count; n++)
+                    {
+                        if(temp_length === 0)
+                        {
+                            var temp_rotation = 2 * Math.PI * Math.random();
+                            var temp_radius = world_radius * Math.sqrt(Math.random());
+                            this.nodes.push(
+                            {
+                                x: temp_radius * Math.cos(temp_rotation),
+                                y: temp_radius * Math.sin(temp_rotation),
+                                r: 2 * Math.PI * Math.random()
+                            });
+                        }
+                        
+                        else
+                        {
+                            var temp_last_node = this.nodes[temp_length - 1];
+                            this.nodes.push(
+                            {
+                                x: temp_last_node.x - 5 * Math.cos(temp_last_node.r),
+                                y: temp_last_node.y + 5 * Math.sin(temp_last_node.r),
+                                r: temp_last_node.r
+                            });
+                        }
+                        
+                        temp_length++;
+                    }
+                }
+                
+                subtractNode(count)
+                {
+                    if(count === undefined)
+                    {
+                        count = 1;
+                    }
+                    
+                    this.nodes.splice(this.nodes.length - count, count);
+                }
+                
+                move()
+                {
+                    if(this.nodes.length > 0)
+                    {
+                        var temp_first_node = this.nodes[0];
+                        temp_first_node.x += 3 * Math.cos(temp_first_node.r);
+                        temp_first_node.y -= 3 * Math.sin(temp_first_node.r);
+                        
+                        for(var n = 1; n < this.nodes.length; n++)
+                        {
+                            var temp_current_node = this.nodes[n];
+                            var temp_next_node = this.nodes[n - 1];
+                            temp_current_node.r = Math.PI - Math.atan2(temp_current_node.y - temp_next_node.y, temp_current_node.x - temp_next_node.x);
+                            temp_current_node.x = temp_next_node.x - 5 * Math.cos(temp_current_node.r);
+                            temp_current_node.y = temp_next_node.y + 5 * Math.sin(temp_current_node.r);
+                        }
+                        
+                        this.camera.x = temp_first_node.x;
+                        this.camera.y = temp_first_node.y;
+                    }
+                }
+            }
+            
+            class Energy
+            {
+                constructor()
+                {
+                    this.type = Math.round(Math.random());
+                    var temp_rotation = 2 * Math.PI * Math.random();
+                    var temp_radius = world_radius * Math.sqrt(Math.random());
+                    this.x = temp_radius * Math.cos(temp_rotation);
+                    this.y = temp_radius * Math.sin(temp_rotation);
+                    this.r = 2 * Math.PI * Math.random();
+                }
+            }
+            
+            class Camera
+            {
+                constructor()
+                {
+                    this.x = null;
+                    this.y = null;
+                }
+            }
+            
             function resize()
             {
                 if(window.innerWidth / window.innerHeight > canvas.width / canvas.height)
@@ -72,37 +187,22 @@
             document.onkeyup = keyup;
             
             var keys = [];
-            var world_radius = 10000;
+            var world_radius = 1000;
             var grid_size = 100;
-            var worm_length = 100;
+            var worm_bot_count = 10;
             var energy_count = 100;
-            var camera =
+            var camera = new Camera();
+            var worms = [new Worm("Human", Math.round(Math.random() * 50 + 20))];
+            var energies = [];
+            
+            for(var n = 0; n < worm_bot_count; n++)
             {
-                x: null,
-                y: null,
-            };
-            var worm =
-            {
-                nodes: [],
-            };
-            var angle = 2 * Math.PI * Math.random();
-            var radius = world_radius * Math.sqrt(Math.random());
-            worm.nodes.push({x: radius * Math.cos(angle), y: radius * Math.sin(angle), r: 2 * Math.PI * Math.random()});
-            for(var n = 0; n < worm_length - 1; n++)
-            {
-                addNode();
-            }
-            var energy = [];
-            for(var n = 0; n < energy_count; n++)
-            {
-                var angle = 2 * Math.PI * Math.random();
-                var radius = world_radius * Math.sqrt(Math.random());
-                energy.push({type: Math.round(Math.random() * 1), x: radius * Math.cos(angle), y: radius * Math.sin(angle), r: 2 * Math.PI * Math.random()});
+                worms.push(new Worm("Bot", Math.round(Math.random() * 50 + 20)));
             }
             
-            function addNode()
+            for(var n = 0; n < energy_count; n++)
             {
-                worm.nodes.push({x: worm.nodes[worm.nodes.length - 1].x - 5 * Math.cos(worm.nodes[worm.nodes.length - 1].r), y: worm.nodes[worm.nodes.length - 1].y + 5 * Math.sin(worm.nodes[worm.nodes.length - 1].r), r: worm.nodes[worm.nodes.length - 1].r});
+                energies.push(new Energy());
             }
             
             var canvas = document.getElementById("canvas");
@@ -112,49 +212,47 @@
             
             function render()
             {
-                if(keys.includes(65) || keys.includes(37))
+                for(var n = 0; n < worms.length; n++)
                 {
-                    worm.nodes[0].r += Math.PI / 60;
+                    var worm = worms[n];
                     
-                    if(worm.nodes[0].r >= 2 * Math.PI)
+                    if(worm.controllable)
                     {
-                        worm.nodes[0].r -= 2 * Math.PI;
+                        if(worm.nodes.length > 0)
+                        {
+                            if(keys.includes(65) || keys.includes(37))
+                            {
+                                worm.nodes[n].r += Math.PI / 60;
+                                
+                                if(worm.nodes[n].r >= 2 * Math.PI)
+                                {
+                                    worm.nodes[n].r -= 2 * Math.PI;
+                                }
+                            }
+                            
+                            if(keys.includes(68) || keys.includes(39))
+                            {
+                                worm.nodes[n].r -= Math.PI / 60;
+                                
+                                if(worm.nodes[n].r < 0)
+                                {
+                                    worm.nodes[n].r += 2 * Math.PI;
+                                }
+                            }
+                        }
                     }
-                }
-                
-                if(keys.includes(68) || keys.includes(39))
-                {
-                    worm.nodes[0].r -= Math.PI / 60;
                     
-                    if(worm.nodes[0].r < 0)
+                    else
                     {
-                        worm.nodes[0].r += 2 * Math.PI;
+                        //AI code
                     }
+                    
+                    worm.move();
                 }
                 
-                //movement goes here
+                camera.x = worms[0].camera.x - canvas.width / 2;
+                camera.y = worms[0].camera.y - canvas.height / 2;
                 
-                worm.nodes[0].x += 3 * Math.cos(worm.nodes[0].r);
-                worm.nodes[0].y -= 3 * Math.sin(worm.nodes[0].r);
-                for(var n = 1; n < worm.nodes.length; n++)
-                {
-                    worm.nodes[n].r = Math.PI - Math.atan2(worm.nodes[n].y - worm.nodes[n - 1].y, worm.nodes[n].x - worm.nodes[n - 1].x);
-                    worm.nodes[n].x = worm.nodes[n - 1].x - 5 * Math.cos(worm.nodes[n].r);
-                    worm.nodes[n].y = worm.nodes[n - 1].y + 5 * Math.sin(worm.nodes[n].r);
-                }
-                for(var n = 0; n < energy.length; n++)
-                {
-                    if(d(worm.nodes[0], energy[n]) < 50)
-                    {
-                        energy.splice(n, 1);
-                        addNode();
-                        n--;
-                    }
-                }
-                camera.x = worm.nodes[0].x - canvas.width / 2;
-                camera.y = worm.nodes[0].y - canvas.height / 2;
-                
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.fillStyle = "#000000";
                 ctx.shadowBlur = 0;
                 ctx.globalAlpha = 1;
@@ -192,57 +290,62 @@
                 ctx.arc(0 - camera.x, 0 - camera.y, world_radius, 0, 2 * Math.PI);
                 ctx.stroke();
                 ctx.closePath();
-                
                 ctx.lineWidth = 3;
                 ctx.shadowBlur = 20;
-                for(var n = 0; n < energy.length; n++)
+                for(var n = 0; n < energies.length; n++)
                 {
+                    var energy = energies[n];
                     ctx.beginPath();
-                    
-                    if(energy[n].type === 0)
+                    if(energy.type === 0)
                     {
                         ctx.strokeStyle = "#ff0000";
                         ctx.shadowColor = ctx.strokeStyle;
-                        ctx.moveTo(energy[n].x + 25 * Math.cos(energy[n].r) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r) - camera.y);
-                        ctx.lineTo(energy[n].x + 25 * Math.cos(energy[n].r + 2 * Math.PI / 3) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r + 2 * Math.PI / 3) - camera.y);
-                        ctx.lineTo(energy[n].x + 25 * Math.cos(energy[n].r + 4 * Math.PI / 3) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r + 4 * Math.PI / 3) - camera.y);
+                        ctx.moveTo(energy.x + 25 * Math.cos(energy.r) - camera.x, energy.y + 25 * Math.sin(energy.r) - camera.y);
+                        ctx.lineTo(energy.x + 25 * Math.cos(energy.r + 2 * Math.PI / 3) - camera.x, energy.y + 25 * Math.sin(energy.r + 2 * Math.PI / 3) - camera.y);
+                        ctx.lineTo(energy.x + 25 * Math.cos(energy.r + 4 * Math.PI / 3) - camera.x, energy.y + 25 * Math.sin(energy.r + 4 * Math.PI / 3) - camera.y);
                     }
                     
-                    if(energy[n].type === 1)
+                    if(energy.type === 1)
                     {
                         ctx.strokeStyle = "#00e5ff";
                         ctx.shadowColor = ctx.strokeStyle;
-                        ctx.moveTo(energy[n].x + 25 * Math.cos(energy[n].r) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r) - camera.y);
-                        ctx.lineTo(energy[n].x + 25 * Math.cos(energy[n].r + Math.PI / 2) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r + Math.PI / 2) - camera.y);
-                        ctx.lineTo(energy[n].x + 25 * Math.cos(energy[n].r + Math.PI) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r + Math.PI) - camera.y);
-                        ctx.lineTo(energy[n].x + 25 * Math.cos(energy[n].r + 3 * Math.PI / 2) - camera.x, energy[n].y + 25 * Math.sin(energy[n].r + 3 * Math.PI / 2) - camera.y);
+                        ctx.moveTo(energy.x + 25 * Math.cos(energy.r) - camera.x, energy.y + 25 * Math.sin(energy.r) - camera.y);
+                        ctx.lineTo(energy.x + 25 * Math.cos(energy.r + Math.PI / 2) - camera.x, energy.y + 25 * Math.sin(energy.r + Math.PI / 2) - camera.y);
+                        ctx.lineTo(energy.x + 25 * Math.cos(energy.r + Math.PI) - camera.x, energy.y + 25 * Math.sin(energy.r + Math.PI) - camera.y);
+                        ctx.lineTo(energy.x + 25 * Math.cos(energy.r + 3 * Math.PI / 2) - camera.x, energy.y + 25 * Math.sin(energy.r + 3 * Math.PI / 2) - camera.y);
                     }
                     
                     ctx.closePath();
                     ctx.stroke();
                 }
                 
-                ctx.strokeStyle = "#00ff00";
-                ctx.shadowColor = ctx.strokeStyle;
-                ctx.beginPath();
-                ctx.arc(worm.nodes[0].x - camera.x, worm.nodes[0].y - camera.y, 25, -(worm.nodes[0].r + Math.PI / 2), -(worm.nodes[0].r - Math.PI / 2));
-                for(var n = 1; n < worm.nodes.length - 1; n++)
+                for(var m = 0; m < worms.length; m++)
                 {
-                    ctx.lineTo(worm.nodes[n].x + 25 * Math.cos(worm.nodes[n].r - Math.PI / 2) - camera.x, worm.nodes[n].y - 25 * Math.sin(worm.nodes[n].r - Math.PI / 2) - camera.y);
+                    var worm = worms[m];
+                    if(worm.nodes.length > 0)
+                    {
+                        ctx.strokeStyle = worm.color;
+                        ctx.shadowColor = ctx.strokeStyle;
+                        ctx.beginPath();
+                        ctx.arc(worm.nodes[0].x - camera.x, worm.nodes[0].y - camera.y, 25, -(worm.nodes[0].r + Math.PI / 2), -(worm.nodes[0].r - Math.PI / 2));
+                        for(var n = 1; n < worm.nodes.length - 1; n++)
+                        {
+                            ctx.lineTo(worm.nodes[n].x + 25 * Math.cos(worm.nodes[n].r - Math.PI / 2) - camera.x, worm.nodes[n].y - 25 * Math.sin(worm.nodes[n].r - Math.PI / 2) - camera.y);
+                        }
+                        ctx.arc(worm.nodes[worm.nodes.length - 1].x - camera.x, worm.nodes[worm.nodes.length - 1].y - camera.y, 25, -(worm.nodes[worm.nodes.length - 1].r - Math.PI / 2), -(worm.nodes[worm.nodes.length - 1].r + Math.PI / 2));
+                        for(var n = worm.nodes.length - 2; n > 0; n--)
+                        {
+                            ctx.lineTo(worm.nodes[n].x + 25 * Math.cos(worm.nodes[n].r + Math.PI / 2) - camera.x, worm.nodes[n].y - 25 * Math.sin(worm.nodes[n].r + Math.PI / 2) - camera.y);
+                        }
+                        ctx.closePath();
+                        ctx.stroke();
+                    }
                 }
-                ctx.arc(worm.nodes[worm.nodes.length - 1].x - camera.x, worm.nodes[worm.nodes.length - 1].y - camera.y, 25, -(worm.nodes[worm.nodes.length - 1].r - Math.PI / 2), -(worm.nodes[worm.nodes.length - 1].r + Math.PI / 2));
-                for(var n = worm.nodes.length - 2; n > 0; n--)
-                {
-                    ctx.lineTo(worm.nodes[n].x + 25 * Math.cos(worm.nodes[n].r + Math.PI / 2) - camera.x, worm.nodes[n].y - 25 * Math.sin(worm.nodes[n].r + Math.PI / 2) - camera.y);
-                }
-                ctx.closePath();
-                ctx.stroke();
-                
                 ctx.shadowBlur = 0;
                 ctx.globalAlpha = 0.25;
-                for(var n = 1; n < 216; n++)
+                for(var n = 1; n < canvas.height / 4; n++)
                 {
-                    ctx.fillRect(0, n * canvas.height / 216 - 1, canvas.width, 2);
+                    ctx.fillRect(0, n * 4 - 1, canvas.width, 2);
                 }
                 
                 var current_time = new Date();
